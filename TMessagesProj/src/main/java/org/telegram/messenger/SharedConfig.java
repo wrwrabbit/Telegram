7a7c8e819a7c9f23ba9aec493741b42804cb7ca7
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-04-09 14:28:20 +0300

    prioritize activated passcode in checkPasscode

--- TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/SharedConfig.java
@@ -1003,7 +1003,7 @@ public class SharedConfig {
                     System.arraycopy(passcodeBytes, 0, bytes, 16, passcodeBytes.length);
                     System.arraycopy(passcodeSalt, 0, bytes, passcodeBytes.length + 16, 16);
                     String hash = Utilities.bytesToHex(Utilities.computeSHA256(bytes, 0, bytes.length));
-                    if (originalPasscodePrioritized && passcodeHash.equals(hash)) {
+                    if ((originalPasscodePrioritized || !FakePasscodeUtils.isFakePasscodeActivated()) && passcodeHash.equals(hash)) {
                         return new PasscodeCheckResult(true, null);
                     }
                     if (FakePasscodeUtils.isFakePasscodeActivated() && FakePasscodeUtils.getActivatedFakePasscode().passcodeHash.equals(hash)) {
