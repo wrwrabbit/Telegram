a3df0f0108a83d69cc48f33f7708591ce7466beb
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-04-26 16:14:45 +0300

    fix fuild

--- TMessagesProj/src/main/java/org/telegram/ui/Components/FolderBottomSheet.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Components/FolderBottomSheet.java
@@ -42,6 +42,7 @@ import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.NotificationCenter;
 import org.telegram.messenger.R;
+import org.telegram.messenger.UserConfig;
 import org.telegram.messenger.UserObject;
 import org.telegram.messenger.Utilities;
 import org.telegram.messenger.fakepasscode.FakePasscodeUtils;
