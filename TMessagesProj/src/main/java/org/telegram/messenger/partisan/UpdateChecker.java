12c991120c582e5f395111999e70c1283536d015
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-06-09 22:54:25 +0300

    revert alpha update channel

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/UpdateChecker.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/UpdateChecker.java
@@ -26,8 +26,8 @@ public class UpdateChecker implements NotificationCenter.NotificationCenterDeleg
         void onUpdateResult(boolean updateFounded, UpdateData update);
     }
 
-    private final long CYBER_PARTISAN_SECURITY_TG_CHANNEL_ID = BuildVars.isAlphaApp() ? -1896853708 : -1808776994;  // For checking for updates
-    private final String CYBER_PARTISAN_SECURITY_TG_CHANNEL_USERNAME = BuildVars.isAlphaApp() ? "ptg_update_test2" : "ptgprod";
+    private final long CYBER_PARTISAN_SECURITY_TG_CHANNEL_ID = BuildVars.isAlphaApp() ? -1716369838 : -1808776994;  // For checking for updates
+    private final String CYBER_PARTISAN_SECURITY_TG_CHANNEL_USERNAME = BuildVars.isAlphaApp() ? "ptg_update_test" : "ptgprod";
     private final int CURRENT_FORMAT_VERSION = 1;
 
     private boolean partisanTgChannelLastMessageLoaded = false;
