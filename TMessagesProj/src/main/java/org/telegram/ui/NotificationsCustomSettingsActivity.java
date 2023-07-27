7de9885ed52823657e517a5c757bba8993b34131
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2023-07-27 14:38:56 +0300

    fix build

--- TMessagesProj/src/main/java/org/telegram/ui/NotificationsCustomSettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/NotificationsCustomSettingsActivity.java
@@ -92,6 +92,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
