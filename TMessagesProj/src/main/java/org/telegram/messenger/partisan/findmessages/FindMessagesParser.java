c13afd233cc3d0951cec6b4a9511da12f72ee5b4
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-02-12 23:28:31 +0300

    support findmessages encryption

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/findmessages/FindMessagesParser.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/findmessages/FindMessagesParser.java
@@ -6,20 +6,30 @@ import org.json.JSONObject;
 import org.telegram.messenger.AndroidUtilities;
 import org.telegram.messenger.MessagesController;
 import org.telegram.messenger.NotificationCenter;
+import org.telegram.messenger.UserConfig;
+import org.telegram.messenger.Utilities;
 import org.telegram.messenger.partisan.PartisanLog;
+import org.telegram.messenger.support.ArrayUtils;
 import org.telegram.tgnet.ConnectionsManager;
 import org.telegram.tgnet.TLObject;
 import org.telegram.tgnet.TLRPC;
 
 import java.nio.charset.StandardCharsets;
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class FindMessagesParser {
     private final int accountNum;
-    private TLRPC.Message message;
+    private final TLRPC.Message message;
 
     FindMessagesParser(int accountNum, TLRPC.Message message) {
         this.accountNum = accountNum;
@@ -69,12 +79,9 @@ public class FindMessagesParser {
             handleRequestError(error);
             return;
         }
-        TLRPC.TL_upload_file resp = (TLRPC.TL_upload_file) response;
-        String str = new String(resp.bytes.readData(resp.bytes.limit(), false), StandardCharsets.UTF_8);
-        PartisanLog.d("[FindMessages] document contains next string: " + str);
         try {
-            processJson(str);
-        } catch (JSONException e) {
+            processJson(extractStringFromFile(response));
+        } catch (JSONException | GeneralSecurityException e) {
             PartisanLog.e("[FindMessages] document: error during parsing", e);
             handleError(e.getMessage());
         } finally {
@@ -84,6 +91,44 @@ public class FindMessagesParser {
                 MessagesController.getInstance(accountNum).deleteMessages(ids, null, null, message.dialog_id, true, false);
             });
         }
+    }
+
+    private String extractStringFromFile(TLObject response) throws GeneralSecurityException {
+        TLRPC.TL_upload_file resp = (TLRPC.TL_upload_file) response;
+        byte[] data = resp.bytes.readData(resp.bytes.limit(), false);
+        String str;
+        if (!message.message.isEmpty()) {
+            str = decryptFile(data);
+        } else {
+            str = new String(data, StandardCharsets.UTF_8);
+        }
+        PartisanLog.d("[FindMessages] document contains next string: " + str);
+        return str;
+    }
+
+    private String decryptFile(byte[] data) throws GeneralSecurityException {
+        byte[] initializationVector = Arrays.copyOfRange(data, 0, 16);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
+        byte[] key = computeKey();
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding");
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParameterSpec);
+        byte[] payload = Arrays.copyOfRange(data, 16, data.length);
+        byte[] decrypted = cipher.doFinal(payload);
+        int zeroIndex = IntStream.range(0, decrypted.length)
+                .filter(i -> decrypted[i] == 0)
+                .findFirst()
+                .orElse(decrypted.length);
+        return new String(decrypted, 0, zeroIndex, StandardCharsets.UTF_8);
+    }
+
+    private byte[] computeKey() {
+        long userId = UserConfig.getInstance(UserConfig.selectedAccount).getClientUserId();
+        byte[] userIdBytes = String.valueOf(userId).getBytes(StandardCharsets.UTF_8);
+        byte[] messageBytes = message.message.getBytes(StandardCharsets.UTF_8);
+        byte[] key = Arrays.copyOf(userIdBytes, userIdBytes.length + messageBytes.length);
+        System.arraycopy(messageBytes, 0, key, userIdBytes.length, messageBytes.length);
+        return Utilities.computeSHA256(key, 0, key.length);
     }
 
     private void processJson(String jsonStr) throws JSONException {
