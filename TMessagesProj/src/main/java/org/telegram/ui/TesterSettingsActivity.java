16bf1f4b1fc8f1064a2dba5062242dbf8838da32
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-05-26 16:50:07 +0300

    remove reset update row from tester settings

--- TMessagesProj/src/main/java/org/telegram/ui/TesterSettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/TesterSettingsActivity.java
@@ -89,7 +89,6 @@ public class TesterSettingsActivity extends BaseFragment {
     private int resetSecurityIssuesRow;
     private int activateAllSecurityIssuesRow;
     private int editSavedChannelsRow;
-    private int resetUpdateRow;
     private int checkVerificationUpdatesRow;
     private int resetVerificationLastCheckTimeRow;
     private int forceAllowScreenshotsRow;
@@ -269,10 +268,6 @@ public class TesterSettingsActivity extends BaseFragment {
                 };
                 AlertDialog dialog = FakePasscodeDialogBuilder.build(getParentActivity(), template);
                 showDialog(dialog);
-            } else if (position == resetUpdateRow) {
-                SharedConfig.pendingPtgAppUpdate = null;
-                SharedConfig.saveConfig();
-                Toast.makeText(getParentActivity(), "Reset", Toast.LENGTH_SHORT).show();
             } else if (position == checkVerificationUpdatesRow) {
                 VerificationUpdatesChecker.checkUpdate(currentAccount, true);
                 Toast.makeText(getParentActivity(), "Check started", Toast.LENGTH_SHORT).show();
@@ -314,7 +309,6 @@ public class TesterSettingsActivity extends BaseFragment {
         resetSecurityIssuesRow = rowCount++;
         activateAllSecurityIssuesRow = rowCount++;
         editSavedChannelsRow = rowCount++;
-        resetUpdateRow = rowCount++;
         checkVerificationUpdatesRow = rowCount++;
         resetVerificationLastCheckTimeRow = rowCount++;
         forceAllowScreenshotsRow = rowCount++;
@@ -448,8 +442,6 @@ public class TesterSettingsActivity extends BaseFragment {
                         textCell.setText("Activate All Security Issues", true);
                     } else if (position == editSavedChannelsRow) {
                         textCell.setTextAndValue("Saved Channels", Integer.toString(getUserConfig().savedChannels.size()), true);
-                    } else if (position == resetUpdateRow) {
-                        textCell.setText("Reset Update", true);
                     } else if (position == checkVerificationUpdatesRow) {
                         textCell.setText("Check Verification Updates", true);
                     } else if (position == resetVerificationLastCheckTimeRow) {
@@ -470,8 +462,7 @@ public class TesterSettingsActivity extends BaseFragment {
                     || (simpleDataStartRow <= position && position < simpleDataEndRow)
                     || position == phoneOverrideRow || position == resetSecurityIssuesRow
                     || position == activateAllSecurityIssuesRow || position == editSavedChannelsRow
-                    || position == resetUpdateRow || position == checkVerificationUpdatesRow
-                    || position == resetVerificationLastCheckTimeRow) {
+                    || position == checkVerificationUpdatesRow || position == resetVerificationLastCheckTimeRow) {
                 return 1;
             }
             return 0;
