f50f9d7ad0ad29e0185ed3726d0760e5b6451b18
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-06-03 18:07:25 +0300

    fix removeNewMessages

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/messageinterception/HiddenByPasscodeMessageInterceptor.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/messageinterception/HiddenByPasscodeMessageInterceptor.java
@@ -15,6 +15,21 @@ public class HiddenByPasscodeMessageInterceptor implements MessageInterceptor {
         if (passcode == null) {
             return false;
         }
-        return passcode.isPreventMessageSaving(accountNum, message.dialog_id);
+        return passcode.isPreventMessageSaving(accountNum, getMessageDialogId(message));
+    }
+
+    private long getMessageDialogId(TLRPC.Message message) {
+        if (message.dialog_id != 0) {
+            return message.dialog_id;
+        } else if (message.from_id != null) {
+            if (message.from_id instanceof TLRPC.TL_peerUser) {
+                return message.from_id.user_id;
+            } else if (message.from_id instanceof TLRPC.TL_peerChannel) {
+                return -message.from_id.channel_id;
+            } else if (message.from_id instanceof TLRPC.TL_peerChat) {
+                return -message.from_id.chat_id;
+            }
+        }
+        return 0;
     }
 }
