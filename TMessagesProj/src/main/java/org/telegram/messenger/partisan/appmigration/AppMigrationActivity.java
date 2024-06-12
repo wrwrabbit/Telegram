69c60533d802aa74722f36031400e61e2ca45ec2
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-06-12 20:30:29 +0300

    fix updating ui in app migrator activity

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/appmigration/AppMigrationActivity.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/appmigration/AppMigrationActivity.java
@@ -57,6 +57,7 @@ public class AppMigrationActivity extends BaseFragment implements AppMigrator.Ma
         }
 
         new Thread(this::checkThread).start();
+        updateUI();
 
         return fragmentView;
     }
