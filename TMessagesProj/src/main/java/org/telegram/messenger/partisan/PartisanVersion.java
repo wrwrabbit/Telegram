697aadf82dfcbe6196090494af2cf3231dc9a37c
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-07-19 21:03:39 +0300

    add identification

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/PartisanVersion.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/PartisanVersion.java
@@ -1,6 +1,6 @@
 package org.telegram.messenger.partisan;
 
 public class PartisanVersion {
-    public static final String PARTISAN_VERSION_STRING = "3.7.1";
+    public static final String PARTISAN_VERSION_STRING = "3.7.1 bug 2.3";
     public static int PARTISAN_BUILD_VERSION = 1452;
 }
