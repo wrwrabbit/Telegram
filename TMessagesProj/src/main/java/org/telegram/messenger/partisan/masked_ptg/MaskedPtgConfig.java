141b028a4d353324d5c2bc3d2421726b839ea96d
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-07-23 15:17:05 +0300

    use NotePasscodeScreen

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/masked_ptg/MaskedPtgConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/masked_ptg/MaskedPtgConfig.java
@@ -2,12 +2,11 @@ package org.telegram.messenger.partisan.masked_ptg;
 
 import android.content.Context;
 
-import org.telegram.messenger.partisan.masked_ptg.loading.LoadingPasscodeScreen;
-import org.telegram.messenger.partisan.masked_ptg.original.OriginalPasscodeScreen;
+import org.telegram.messenger.partisan.masked_ptg.note.NotePasscodeScreen;
 
 public class MaskedPtgConfig {
     public static MaskedPasscodeScreen createScreen(Context context, PasscodeEnteredDelegate delegate) {
-        return new OriginalPasscodeScreen(context, delegate);
+        return new NotePasscodeScreen(context, delegate);
     }
 
     public static boolean allowAlphaNumericPassword() {
