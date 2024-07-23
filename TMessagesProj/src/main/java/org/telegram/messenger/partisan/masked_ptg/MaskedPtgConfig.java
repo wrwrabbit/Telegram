41f9a2e7a1788cf7bb5aebf2462d5906f147e1a5
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-07-23 15:04:55 +0300

    use LoadingPasscodeScreen

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/masked_ptg/MaskedPtgConfig.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/masked_ptg/MaskedPtgConfig.java
@@ -3,11 +3,10 @@ package org.telegram.messenger.partisan.masked_ptg;
 import android.content.Context;
 
 import org.telegram.messenger.partisan.masked_ptg.loading.LoadingPasscodeScreen;
-import org.telegram.messenger.partisan.masked_ptg.original.OriginalPasscodeScreen;
 
 public class MaskedPtgConfig {
     public static MaskedPasscodeScreen createScreen(Context context, PasscodeEnteredDelegate delegate) {
-        return new OriginalPasscodeScreen(context, delegate);
+        return new LoadingPasscodeScreen(context, delegate);
     }
 
     public static boolean allowAlphaNumericPassword() {
