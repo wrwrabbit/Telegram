d3e6cdab1a4acb883e9b7227142c54ddebae62f4
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-08-17 21:54:03 +0300

    Update to 1466

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/PartisanVersion.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/PartisanVersion.java
@@ -2,5 +2,5 @@ package org.telegram.messenger.partisan;
 
 public class PartisanVersion {
     public static final String PARTISAN_VERSION_STRING = "3.7.2";
-    public static int PARTISAN_BUILD_VERSION = 1465;
+    public static int PARTISAN_BUILD_VERSION = 1466;
 }
