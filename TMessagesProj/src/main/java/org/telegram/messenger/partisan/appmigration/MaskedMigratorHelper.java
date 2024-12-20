9b1021f03262c8faa1aa3d411838d803ce28ccf6
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2024-12-20 05:03:14 +0300

    change masking bot ids

--- TMessagesProj/src/main/java/org/telegram/messenger/partisan/appmigration/MaskedMigratorHelper.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/partisan/appmigration/MaskedMigratorHelper.java
@@ -10,9 +10,9 @@ import java.util.Map;
 
 public class MaskedMigratorHelper {
     public static String MASKING_BOT_USERNAME = null;
-    public static final long MASKING_BOT_ID = 7116474629L;
+    public static final long MASKING_BOT_ID = 7901437172L;
     private static final long MASKING_BOT_ID2 = 7138739692L;
-    private static final long MASKING_BOT_ID3 = 7901437172L;
+    private static final long MASKING_BOT_ID3 = 7116474629L;
     private static final Map<File, PackageInfo> fileToPackageInfo = new HashMap<>();
     private static PackageInfo installingPackageInfo = null;
 
