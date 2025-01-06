2a960a77d6268cc91ee8e32c4ecfec260e822d2b
vivabelarus <74508922+vivabelarus@users.noreply.github.com>
2025-01-06 13:04:42 +0300

    DEBUG_VERSION = true

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -18,7 +18,7 @@ import java.util.Objects;
 
 public class BuildVars {
 
-    public static boolean DEBUG_VERSION = BuildConfig.DEBUG_VERSION;
+    public static boolean DEBUG_VERSION = true;
     public static boolean LOGS_ENABLED = BuildConfig.DEBUG_VERSION;
     public static boolean DEBUG_PRIVATE_VERSION = BuildConfig.DEBUG_PRIVATE_VERSION;
     public static boolean USE_CLOUD_STRINGS = true;
